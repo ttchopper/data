e093bafbf1aefab94ecd3892723af76788988114
andrew <twinklechopper@gmail.com>
2016-08-01 16:11:49 +0300

    Initial Commit

---
+++ database.js
@@ -0,0 +1,61 @@
+var mainProject = new Schema({
+    Users: [{
+        id: Schema.User.ObjectId, 
+        name: String,
+        required: true
+    }],
+
+    Techologies: [{
+        name: {type: String, require: true},
+        id: ObjectId,
+        required: true
+    }],
+
+    Name: {
+        type: String,
+        required: true    
+    },
+
+    Description: String,
+
+    ScreenShots: {
+        id: ObjectId,
+        type: Buffer
+    },
+
+    TimeBegin: Date,
+
+    TimeEnd: Date,
+
+    Tags: [{String}],
+
+    Stage: {
+        type: Boolean
+        image: {type: Buffer, id: ObjectId}
+    }
+
+});
+
+var reqProject = new Schema({
+    Name: String,
+    Description: String,
+    Tags: String,
+
+    Techologies: [{
+        name: {type: String, require: true},
+        id: ObjectId,
+        required: true
+    }],
+
+
+    ScreenShots: {
+        id: ObjectId,
+        type: Buffer
+    },
+
+    Stage: [{
+        in_progress: Boolean,
+        Estimated: Boolean,
+        Discussed: Boolean
+    }]
+});
